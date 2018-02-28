715c9267bba2e789cdc4cdbe6371a365088274d7
maple3142 <kirby741852963@gmail.com>
2018-02-28 12:01:03 +0800

    remove IIFE

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ts from './handlers/ts'
 import mp4 from './handlers/mp4'
 import torrent from './handlers/torrent'
 import other from './handlers/other'
-;(function($) {
-	const loc = location
-	if (loc.hostname === 'p.anime1.me' && loc.pathname === '/pic.php') pic()
-	else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/ts.php') ts()
-	else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/mp4.php') mp4()
-	else if (loc.hostname === 'torrent.anime1.me') torrent()
-	else other()
-})(jQuery)
+const loc = location
+if (loc.hostname === 'p.anime1.me' && loc.pathname === '/pic.php') pic()
+else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/ts.php') ts()
+else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/mp4.php') mp4()
+else if (loc.hostname === 'torrent.anime1.me') torrent()
+else other()
--- userscript.js
+++ userscript.js
@@ -115,7 +115,5 @@ function other () {
 	}
 }
 
-(function ($) {
-	const loc = location;
-	if (loc.hostname === 'p.anime1.me' && loc.pathname === '/pic.php') pic();else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/ts.php') ts();else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/mp4.php') mp4();else if (loc.hostname === 'torrent.anime1.me') torrent();else other();
-})(jQuery);
+const loc = location;
+if (loc.hostname === 'p.anime1.me' && loc.pathname === '/pic.php') pic();else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/ts.php') ts();else if (loc.hostname === 'p.anime1.me' && loc.pathname === '/mp4.php') mp4();else if (loc.hostname === 'torrent.anime1.me') torrent();else other();
